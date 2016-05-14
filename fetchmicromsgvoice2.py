505edad69a01491740cac31596d7c0ef96c8f11a
Ren Wei (任卫) <wadefelix@gmail.com>
2016-05-14 13:03:12 +0800

    直接从文件名解析时间信息

--- fetchmicromsgvoice2.py
+++ fetchmicromsgvoice2.py
@@ -3,6 +3,9 @@
 
 from ftplib import FTP
 import os
+import datetime
+import time
+
 localdir = r'D:\renxiaoan\micromsg'
 
 ftp=FTP()
@@ -27,8 +30,14 @@ def get_voicedirname(line):
 
 ftp.dir(get_file_list)
 
+
+# -rw-r--r-- 1 owner group       668032 Jan 14 16:19 1452759557133_0f13343d.jpg
+# 04261951msg_121951042616ea710c10030103.amr
+# 秒小时分钟月日两位年
+
+
 for voicedirname in voicedirnames:
-    os.mkdir(os.path.join(localdir, voicedirname ))
+    if not os.path.exists(os.path.join(localdir, voicedirname )): os.mkdir(os.path.join(localdir, voicedirname ))
     ftp.cwd(voicedirname)
     for dirname in ftp.nlst():
         if dirname == 'voice2':
@@ -37,11 +46,13 @@ for voicedirname in voicedirnames:
                 ftp.cwd(dirname2)
                 for dirname3 in ftp.nlst():
                     ftp.cwd(dirname3)
+                    #print os.path.join(voicedirname, dirname, dirname2, dirname3)
                     for dirname4 in ftp.nlst():
                         finfo = dirname4.split('.')
                         if finfo[-1] == 'amr' :
                             print os.path.join(voicedirname, dirname, dirname2, dirname3 ,dirname4)
-                            localfile = os.path.join(localdir, voicedirname ,dirname4)
+                            newname = dirname4[10:12] + dirname4[6:10] + dirname4[2:6] + dirname4[:2] + dirname4[12:] 
+                            localfile = os.path.join(localdir, voicedirname ,newname)
                             file_handler = open(localfile, 'wb')
                             ftp.retrbinary('RETR {}'.format(dirname4), file_handler.write)
                             file_handler.close()
