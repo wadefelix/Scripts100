c15ed96944a4f66bfdab5471d4fd296c7ab55b6a
Ren Wei <wadefelix@gmail.com>
2018-02-01 14:10:41 +0800

    完善这个RSA签名验证加密解密示例

--- signfile.py
+++ signfile.py
@@ -1,25 +1,29 @@
 #! python
-#!-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # --------------------------------------------------------
 # Written by Ren Wei
 # --------------------------------------------------------
 
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from Crypto import Random
 from Crypto.Hash import SHA
 import base64
 import os.path as osp
-
 import argparse
 
 
-'''*RSA签名
-* data待签名数据
-* 签名用商户私钥，必须是没有经过pkcs8转换的私钥
-* 最后的签名，需要用base64编码
-* return Sign签名
-'''
 def sign(data, priKey=None):
+    """
+    使用私钥签名
+    Args:
+        data: 待签名数据
+        priKey: 私钥。默认使用用户的默认私钥 ~/.ssh/id_rsa
+
+    Returns:
+        签名
+    """
     if priKey is None:
         priKey = open(osp.expanduser('~/.ssh/id_rsa'), 'r').read()
     key = RSA.importKey(priKey)
@@ -28,13 +32,18 @@ def sign(data, priKey=None):
     signature = signer.sign(h)
     return base64.b64encode(signature)
 
-'''*RSA验签
-* data待签名数据
-* signature需要验签的签名
-* 验签用公钥
-* return 验签是否通过 bool值
-'''
+
 def verify(data, signature, pubKey=None):
+    """
+    使用公钥验证签名
+    Args:
+        data: 原始数据
+        signature: 签名
+        pubKey: 公钥。默认使用用户的默认公钥 ~/.ssh/id_rsa.pub
+
+    Returns:
+        验证通过与否
+    """
     if pubKey is None:
         pubKey = open(osp.expanduser('~/.ssh/id_rsa.pub'), 'r').read()
     key = RSA.importKey(pubKey)
@@ -45,15 +54,52 @@ def verify(data, signature, pubKey=None):
     return False
 
 
+def encrypt(data, pubKey=None):
+    """
+    使用公钥加密数据
+    Args:
+        data: 需要加密的数据
+        pubKey: 公钥。默认使用用户的默认公钥 ~/.ssh/id_rsa.pub
+
+    Returns:
+        加密后的数据
+    """
+    if pubKey is None:
+        pubKey = open(osp.expanduser('~/.ssh/id_rsa.pub'), 'r').read()
+    key = RSA.importKey(pubKey)
+    cipher = Cipher_pkcs1_v1_5.new(key)
+    cipher_text = base64.b64encode(cipher.encrypt(data))
+    return cipher_text
+
+
+def decrypt(data, priKey=None):
+    """
+    使用私钥解密数据
+    Args:
+        data: 需要解密的数据
+        priKey: 私钥。默认使用用户的默认私钥 ~/.ssh/id_rsa
+
+    Returns:
+        解密后的数据
+    """
+    if priKey is None:
+        priKey = open(osp.expanduser('~/.ssh/id_rsa'), 'r').read()
+    key = RSA.importKey(priKey)
+    cipher = Cipher_pkcs1_v1_5.new(key)
+    random_generator = Random.new().read
+    text = cipher.decrypt(base64.b64decode(data), random_generator)
+    return text
+
+
 def parse_args():
     """
     Parse input arguments
     """
-    parser = argparse.ArgumentParser(description='signfile')
+    parser = argparse.ArgumentParser(description='RSA signature, verify, encrypt and decrypt Samples')
     parser.add_argument('--infile', dest='infile',
-                        help='file to be signed or verified.', type=str)
+                        help='file to be handled.', type=str)
     parser.add_argument('--outfile', dest='outfile',
-                        help='Outputing Signature File',
+                        help='file to be output.',
                         default=None, type=str)
     parser.add_argument('--signature', dest='signature',
                         help='Signature File',
@@ -64,29 +110,48 @@ def parse_args():
     parser.add_argument('--pubkey', dest='pubkeyfile',
                         help='Public Key File',
                         default=None, type=str)
+    parser.add_argument('--encrypt', action="store_true", help='encrypt the infile to outfile')
+    parser.add_argument('--decrypt', action="store_true", help='decrypt the infile to outfile')
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     args = parse_args()
 
     raw_data = open(args.infile, 'rb').read()
+    func = None
+    params = [raw_data]
 
-    if args.signature is not None:
+    if args.encrypt:
+        func = encrypt
         pubkey = None
         if args.pubkeyfile is not None:
             pubkey = open(args.pubkeyfile, 'r').read()
-        print verify(raw_data,
-                     open(args.signature, 'r').read(),
-                     pubkey)
-    else:
+        params.append(pubkey)
+    elif args.decrypt:
+        func = decrypt
         prikey = None
         if args.prikeyfile is not None:
             prikey = open(args.prikeyfile, 'r').read()
-        sign_data = sign(raw_data,prikey)
-        if args.outfile is not None:
-            with open(args.outfile,'w') as f:
-                f.write(sign_data)
-        else:
-            print sign_data
+        params.append(prikey)
+    elif args.signature is not None:
+        func = verify
+        pubkey = None
+        if args.pubkeyfile is not None:
+            pubkey = open(args.pubkeyfile, 'r').read()
+        params.append(open(args.signature, 'r').read())
+        params.append(pubkey)
+    else:
+        func = sign
+        prikey = None
+        if args.prikeyfile is not None:
+            prikey = open(args.prikeyfile, 'r').read()
+        params.append(prikey)
 
+    data = func(*params)
+    if args.outfile is not None:
+        with open(args.outfile,'w') as f:
+            f.write(data)
+    else:
+        print data
